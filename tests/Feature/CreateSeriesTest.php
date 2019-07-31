d7de7a5da52ad104e3acdfcbb02805fdd35573a9
xaviranik <xaviranik.me@gmail.com>
2019-07-31 13:38:19 +0600

    Added: Session Flash on SeriesController

--- tests/Feature/CreateSeriesTest.php
+++ tests/Feature/CreateSeriesTest.php
@@ -24,12 +24,46 @@ class CreateSeriesTest extends TestCase
             'title' => $title,
             'description' => "Test Description",
             'image' => UploadedFile::fake()->image('test-image.png')
-        ])->assertRedirect();
+        ])->assertRedirect()
+            ->assertSessionHas('success', 'Series created successfully');
 
         Storage::disk(config('filesystems.default'))->assertExists('series/' . str_slug($title) . '.png');
 
         $this->assertDatabaseHas('series', [
             'slug' => str_slug($title)
         ]);
+    }
+
+    public function test_a_series_must_have_a_title()
+    {
+        $this->post('/admin/series', [
+            'description' => "Test Description",
+            'image' => UploadedFile::fake()->image('test-image.png')
+        ])->assertSessionHasErrors('title');
+    }
+
+    public function test_a_series_must_have_a_description()
+    {
+        $this->post('/admin/series', [
+            'title' => "Test Title",
+            'image' => UploadedFile::fake()->image('test-image.png')
+        ])->assertSessionHasErrors('description');
+    }
+
+    public function test_a_series_must_have_an_image()
+    {
+        $this->post('/admin/series', [
+            'title' => "Test Title",
+            'description' => 'Test Description'
+        ])->assertSessionHasErrors('image');
+    }
+
+    public function test_uploaded_image_must_be_a_valid_image()
+    {
+        $this->post('/admin/series', [
+            'title' => "Test Title",
+            'description' => 'Test Description',
+            'image' => 'invalid-image'
+        ])->assertSessionHasErrors('image');
     }
 }
