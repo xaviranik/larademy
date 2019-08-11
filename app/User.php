061c8cff8dc3c5edf5572af3c00eeeb21639dbc1
Xavir-Linux <xaviranik.me@gmail.com>
2019-08-11 20:02:29 +0600

    Added: UserTest, lesson-series relationship

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Redis;
 
 class User extends Authenticatable
 {
@@ -40,5 +41,10 @@ class User extends Authenticatable
     public function isAdmin()
     {
         return in_array($this->email, config('larademy.administrators'));
+    }
+
+    public function completeLesson($lesson)
+    {
+        Redis::sadd("user:{$this->id}:series:{$lesson->series->id}", $lesson->id);
     }
 }
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Series;
+use App\Lesson;
+use Illuminate\Support\Facades\Redis;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_a_user_can_complete_a_lesson()
+    {
+        $user = factory(User::class)->create();
+        $series = factory(Series::class)->create();
+
+        $lesson = factory(Lesson::class)->create([
+            'series_id' => $series->id
+        ]);
+
+        $user->completeLesson($lesson);
+
+        $this->assertEquals(
+            Redis::smembers("user:1:series:1"), [1]
+        );
+    }
+}
