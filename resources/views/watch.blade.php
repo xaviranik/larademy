3bf525a82fe6d1ccc8dac6cce22e1fb9de9ed780
xaviranik <xaviranik.me@gmail.com>
2019-08-14 20:47:04 +0600

    Added: Next and Previous button for lesson navigation

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -10,6 +10,23 @@
         </div>
     </div>
 
+    <div class="row justify-content-between px-3 py-2">
+        @if ($lesson->getPreviousLesson())
+            <a href="{{ route('series.watch', ['series' => $series->slug, 'lesson' => $lesson->getPreviousLesson()->id]) }}"
+            class="btn btn-primary"><i class="material-icons">chevron_left</i> Previous Lesson</a>
+        @else
+            <a href="#"
+            class="btn btn-primary disabled"><i class="material-icons">chevron_left</i> Previous Lesson</a>  
+        @endif
+        @if ($lesson->getNextLesson())
+            <a href="{{ route('series.watch', ['series' => $series->slug, 'lesson' => $lesson->getNextLesson()->id]) }}"
+            class="btn btn-primary">Next Lesson <i class="material-icons">chevron_right</i></a>
+        @else
+            <a href="#"
+            class="btn btn-outline-primary">Finish Series</a>
+        @endif
+    </div>
+
     <div class="row">
         <div class="col-md-12">
             <div class="card">
