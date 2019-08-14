bdb0f8c3a3369eb4844c33fda55449f62ad92d41
Xavir-Linux <xaviranik.me@gmail.com>
2019-08-14 23:01:24 +0600

    Added: All lesson view on watch blade

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -12,27 +12,41 @@
 
     <div class="row justify-content-between px-3 py-2">
         @if ($lesson->getPreviousLesson())
-            <a href="{{ route('series.watch', ['series' => $series->slug, 'lesson' => $lesson->getPreviousLesson()->id]) }}"
+        <a href="{{ route('series.watch', ['series' => $series->slug, 'lesson' => $lesson->getPreviousLesson()->id]) }}"
             class="btn btn-primary"><i class="material-icons">chevron_left</i> Previous Lesson</a>
         @else
-            <a href="#"
-            class="btn btn-primary disabled"><i class="material-icons">chevron_left</i> Previous Lesson</a>  
+        <a href="#" class="btn btn-primary disabled"><i class="material-icons">chevron_left</i> Previous Lesson</a>
         @endif
         @if ($lesson->getNextLesson())
-            <a href="{{ route('series.watch', ['series' => $series->slug, 'lesson' => $lesson->getNextLesson()->id]) }}"
+        <a href="{{ route('series.watch', ['series' => $series->slug, 'lesson' => $lesson->getNextLesson()->id]) }}"
             class="btn btn-primary">Next Lesson <i class="material-icons">chevron_right</i></a>
         @else
-            <a href="#"
-            class="btn btn-outline-primary">Finish Series</a>
+        <a href="#" class="btn btn-outline-primary">Finish Series</a>
         @endif
     </div>
 
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
                     <h4 class="title">{{ $lesson->title }}</h4>
                     <p>{{ $lesson->description }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="title mb-0">All Lessons</h4>
+                    <ul class="list-group">
+                        @foreach ($series->getOrderedLessons() as $l)
+                            <li class="list-group-item p-3">
+                                <a style="line-height: initial;" href="{{ route('series.watch', ['series' => $series->slug, 'lesson' => $l->id]) }}">{{ $l->episode_number . ". " .$l->title }}</a>
+                                <hr>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
